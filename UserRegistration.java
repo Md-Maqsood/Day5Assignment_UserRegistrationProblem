1e557d753b80506bbd50cd94dfc437ad06e901ee
maqsood <maqsoodhamd37@gmail.com>
2020-10-02 02:06:04 +0530

    [maqsood] update:solved uc7

--- UserRegistration.java
+++ UserRegistration.java
@@ -37,7 +37,7 @@ public class UserRegistration {
 	}
 	
 	public static boolean validatePassword(String password) {
-		if ((password.length()>=8)&&Pattern.matches("*[A-Z]*", password)) {
+		if ((password.length()>=8)&&Pattern.matches("*[A-Z]*", password)&&Pattern.matches("*[0-9]*", password)) {
 			return true;
 		} else {
 			System.out.println("Invalid entry for a first_name or a last_name");
