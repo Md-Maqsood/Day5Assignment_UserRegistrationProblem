ec83c8168348ddda5d439b6f2ec372f29cd414b2
maqsood <maqsoodhamd37@gmail.com>
2020-10-01 22:46:22 +0530

    [maqsood] update:solved uc2

--- UserRegistration.java
+++ UserRegistration.java
@@ -13,7 +13,7 @@ public class UserRegistration {
 		if (Pattern.matches("^[A-Z]([a-z]{2,}|[A-Z]{2,})", name)) {
 			return true;
 		} else {
-			System.out.println("Invalid entry for a first_name");
+			System.out.println("Invalid entry for a first_name or a last_name");
 			return false;
 		}
 	}
@@ -27,8 +27,10 @@ public class UserRegistration {
 			if (choice == 1) {
 				System.out.println("Enter the first name: ");
 				String firstName = sc.nextLine();
-				if (validateName(firstName)) {
-					userReg.usersList.add(new User(firstName));
+				System.out.println("Enter the last name: ");
+				String lastName = sc.nextLine();
+				if (validateName(firstName) && validateName(lastName)) {
+					userReg.usersList.add(new User(firstName, lastName));
 				}
 			} else if (choice == 2) {
 				break;
@@ -45,14 +47,24 @@ public class UserRegistration {
 
 class User {
 	private String firstName;
+	private String lastName;
 
-	public User(String firstName) {
+	public User(String firstName, String lastName) {
 		super();
 		this.firstName = firstName;
+		this.lastName = lastName;
 	}
 
 	public String getFirstName() {
 		return firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
 	}
 
 	public void setFirstName(String firstName) {
@@ -61,7 +73,7 @@ class User {
 
 	@Override
 	public String toString() {
-		return "User [firstName=" + firstName + "]";
+		return "User [firstName=" + firstName + ", lastName=" + lastName + "]";
 	}
 
 }
